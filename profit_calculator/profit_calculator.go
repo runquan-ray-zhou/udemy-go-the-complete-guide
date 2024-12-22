442fe4494f0531447f9bf88b874187cf3c3a61f9
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-21 19:32:03 -0500

    add information on how to get output, added code to print and scan

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -3,6 +3,15 @@ Ask for revenue, expense & tax rate
 Calculate earnings before tax(EBT) and earnings after tax(profit)
 Calculate ratio (EBT/profit)
 Output EBT, profit and the ratio
+
+Revenue is all income
+EBT is profit after expense
+Earning after taxes(Profit) is profit after taxes
+
+To get EBT you subtract expenses from revenue
+To get Taxes you multiply tax rate to EBT
+To get profit you subtract taxes from EBT
+To get ratio you divide profit into EBT
 */
 
 package main
@@ -10,5 +19,13 @@ package main
 import "fmt"
 
 func main () {
-	fmt.Println("Hello World")
+
+	fmt.Print("What is your total revenue: ")
+	fmt.Scan(&revenue)
+
+	fmt.Print("What is your total expense: ")
+	fmt.Scan(&expense)
+
+	fmt.Print("What is the current tax rate: ")
+	fmt.Scan(&taxRate)
 }
