4eb2debc2fbe821604d49115222ebd6f881e93ad
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-21 19:18:38 -0500

    add program requirements

--- profit_calculator/profit_calculator.go
+++ profit_calculator/profit_calculator.go
@@ -1,3 +1,10 @@
+/*
+Ask for revenue, expense & tax rate
+Calculate earnings before tax(EBT) and earnings after tax(profit)
+Calculate ratio (EBT/profit)
+Output EBT, profit and the ratio
+*/
+
 package main
 
 import "fmt"
