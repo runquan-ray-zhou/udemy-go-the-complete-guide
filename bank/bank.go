f629efac7eb2bb81823a4694ebcc45ab72c5b10b
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-22 01:51:32 -0500

    add if else for withdraw and deposit

--- bank/bank.go
+++ bank/bank.go
@@ -3,10 +3,57 @@ package main
 import "fmt"
 
 func main() {
+
+	var accountBalance = 1000.0
+
+	for i := 0; i < 2;
+
 	fmt.Println("Welcome to Go Bank!")
 	fmt.Println("What do you want to do?")
 	fmt.Println("1. Check balance")
 	fmt.Println("2. Deposit money")
 	fmt.Println("3. Withdraw money")
 	fmt.Println("4. Exit")
+
+	var choice int
+	fmt.Print("Your choice: ")
+	fmt.Scan(&choice)
+
+	// wantsCheckBalance := choice ==1 
+
+	if choice == 1 {
+		fmt.Println("Your balance is", accountBalance)
+	} else if choice == 2 {
+		fmt.Println("Your deposit: ")
+		var depositAmount float64
+		fmt.Scan(&depositAmount)
+		
+		if depositAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0")
+			return
+		}
+		
+		accountBalance += depositAmount
+		fmt.Println("Balance update! New amount:", accountBalance)
+	} else if choice == 3 {
+		fmt.Println("Withdraw amount: ")
+		var withdrawAmount float64
+		fmt.Scan(&withdrawAmount)
+
+		if withdrawAmount <= 0 {
+			fmt.Println("Invalid amount. Must be greater than 0")
+			return
+		}
+
+		if withdrawAmount > accountBalance {
+			fmt.Println("Invalid amount.  You can't withdraw more than you have")
+			return
+		}
+		accountBalance -= accountBalance
+		fmt.Println("Balance update! New amount:", accountBalance)
+	} else {
+		fmt.Println("Good Bye!")
+	}
+
+	fmt.Println("Your choice:", choice)
 }
