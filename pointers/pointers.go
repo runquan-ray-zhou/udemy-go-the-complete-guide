c4a663c65b0bd0a6b6582c3909dc436dca705baf
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-23 21:30:55 -0500

    add pointers code

--- pointers/pointers.go
+++ pointers/pointers.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World.")
+	age := 32 // regular variable
+
+	var agePointer *int
+	agePointer = &age
+	// agePointer := &age
+
+	fmt.Println("Age:", *agePointer)
+
+	editAgeToAdultYears(agePointer)
+	fmt.Println(age)
+}
+
+func editAgeToAdultYears(age *int) {
+	// return *age - 18
+	*age = *age - 18 // change the value
 }
