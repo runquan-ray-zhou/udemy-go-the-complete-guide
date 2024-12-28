196847e7344f16e919f17843bccd477ec6f81e6e
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-28 13:45:15 -0500

    complete price calculator project

--- price_calculator/main.go
+++ price_calculator/main.go
@@ -1,19 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/runquan-ray-zhou/udemy-go-the-complete-guide/price_calculator/cmdmanager"
+	"github.com/runquan-ray-zhou/udemy-go-the-complete-guide/price_calculator/prices"
+)
+
+type IOManager interface {
+	Readlines() ([]string, error)
+	WriteResult(data interface{}) error
+}
 
 func main() {
-	prices := []float64{10, 20, 30}
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 
-	result := make(map[float64][]float64)
-
 	for _, taxRate := range taxRates {
-		taxIncludePrices := make([]float64, len(prices))
-		for priceIndex, price := range prices {
-			taxIncludePrices[priceIndex] = price * (1 + taxRate)
+		// fm := filemanager.New("prices.txt", fmt.Sprintf("result_%0.f.json", taxRate*100))
+		cmdm := cmdmanager.New()
+		priceJob := prices.NewTaxIncludedPriceJob(cmdm, taxRate)
+		err := priceJob.Process()
+
+		if err != nil {
+			fmt.Println("Could not process job")
+			fmt.Println(err)
 		}
-		result[taxRate] = taxIncludePrices
 	}
-	fmt.Println(result)
+
 }
