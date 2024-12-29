f818957342e4592ce7cceecd1f557e220139a220
runquan-ray-zhou <151119924+runquan-ray-zhou@users.noreply.github.com>
2024-12-29 00:23:12 -0500

    update main.go

--- concurrency/main.go
+++ concurrency/main.go
@@ -5,18 +5,36 @@ import (
 	"time"
 )
 
-func greet(phrase string) {
+func greet(phrase string, doneChan chan bool) {
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
 }
 
-func slowGreet(phrase string) {
+func slowGreet(phrase string, doneChan chan bool) {
 	time.Sleep(3 * time.Second) // simulate a slow, long-taking task
 	fmt.Println("Hello!", phrase)
+	doneChan <- true
+	close(doneChan) // only makes sense if you know which application takes the longest
 }
 
 func main() {
-	greet("Nice to meet you!")
-	greet("How are you?")
-	slowGreet("How ... are ... you ...?")
-	greet("I hope you're liking the course!")
+	// dones := make([]chan bool, 4) multiple hannel
+	done := make(chan bool) //contains a channel
+
+	// dones[0] = make(chan bool)
+	go greet("Nice to meet you!", done)
+	// dones[1] = make(chan bool)
+	go greet("How are you?", done)
+	// dones[2] = make(chan bool)
+	go slowGreet("How ... are ... you ...?", done)
+	// dones[3] = make(chan bool)
+	go greet("I hope you're liking the course!", done)
+
+	// for _, done := range dones {
+	// 	<-done
+	// }
+
+	for range done {
+		// fmt.Println(doneChan)
+	}
 }
